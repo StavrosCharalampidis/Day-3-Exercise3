ad9e3ab3ae78a318a2a93008989cd1f559e05093
StavrosCharalampidis <stavroscharalampidis1@gmail.com>
2024-02-02 13:31:55 +0200

    Some Math Complete

---
+++ ex3.py
@@ -0,0 +1,12 @@
+# Print some Float type
+print("what is 212.3 + 150.8: ", 212.3 + 150.8)
+print("what is 10.2 - 15.4: ", 10.2 - 15.4)
+print("what is 5.5 * 2.2: ", 5.5 * 2.2)
+print("what is 12.3 / 3.3: ", 12.3 / 3.3)
+print("what is 12.3 % 3.3: ", 5.5 % 2.2)
+
+# Print some Boolean type 
+print("Is greader", 10 > 5)
+print("Is greader or equal", 10 >= 5)
+print("is less", 10 < 5)
+print("Is less or equal", 10 <= 5)
